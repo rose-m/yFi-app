c17117f67144074606abeba46a83900a7123f11d
rosem <michael_rose@gmx.de>
2020-05-09 15:16:10 +0200

    Initial Commit

---
+++ yFi/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  yFi
+//
+//  Created by Michael Rose on 09.05.20.
+//  Copyright © 2020 Coderose. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
